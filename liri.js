f70f8de42e35ceac8e0349589931d1d2bcaab50b
Oscar Gonzalez <ogonzalez0774@gmail.com>
2019-10-04 17:28:30 -0500

    added the first axios call for concert-this

--- liri.js
+++ liri.js
@@ -1,2 +1,19 @@
 require("dotenv").config();
+const axios = require("axios")
 const keys = require("./keys.js");
+let [, , ...args] = process.argv
+if (args[0] === "concert-this") {
+    axios.get("https://rest.bandsintown.com/artists/" + args[1] + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            let name = response.data[0].venue.name
+            let city = response.data[0].venue.city
+            let country = response.data[0].venue.country
+            let date = response.data[0].datetime
+            console.log(
+                "Name: " + name,
+                "\nLocation: " + city + "," + country,
+                "\nDate: " + date
+            );
+
+        })
+};
