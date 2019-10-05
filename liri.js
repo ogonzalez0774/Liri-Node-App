b7a17320c8cf779357d530569faa915fd5b76a81
Oscar Gonzalez <ogonzalez0774@gmail.com>
2019-10-05 17:36:35 -0500

    finished spotify api and started movie api

--- liri.js
+++ liri.js
@@ -1,7 +1,9 @@
 //gloval
 require("dotenv").config();
+let Spotify = require('node-spotify-api');
 const axios = require("axios");
 const keys = require("./keys.js");
+const fs = require("fs")
 let [, , ...args] = process.argv;
 
 // this will determine what the search will be and what to do if nothing is searched
@@ -17,11 +19,9 @@ if (args[0] === "concert-this") {
   } else {
     axios
       .get(
-        "https://rest.bandsintown.com/artists/" +
-          args[1] +
-          "/events?app_id=codingbootcamp"
+        "https://rest.bandsintown.com/artists/" + args[1] + "/events?app_id=codingbootcamp"
       )
-      .then(function(response) {
+      .then(function (response) {
         let name = response.data[0].venue.name;
         let city = response.data[0].venue.city;
         let country = response.data[0].venue.country;
@@ -39,22 +39,57 @@ if (args[0] === "concert-this") {
 // this will determine what the search will be and what to do if nothing is searched
 
 // spotify song search
+if (args[0] === "spotify-this-song") {
+  if (args[0] === "spotify-this-song" && args[1] === undefined) {
+    console.log(
+      "----------------------------",
+      "\nPlease Enter A Song",
+      "\n----------------------------"
+    );
+  } else {
+    let spotify = new Spotify({
+      id: "4e4cfacf4f944db59467b76d1627692b",
+      secret: "40b7c8d69a704900adf7ea9329857328"
+    });
 
+    spotify
+      .search({
+        type: "track",
+        query: args[1]
+      }).then(function (response) {
+        console.log(
+
+          "-----------------------------",
+          // "\n", response.tracks.items[0],
+          "\n", response.tracks.items[0].artists[0].name,
+          "\n", response.tracks.items[0].name,
+          "\n", response.tracks.items[0].preview_url,
+          "\n", response.tracks.items[0].album.name,
+          "\n-----------------------------",
+        )
+      })
+
+  }
+}
 // this will determine what the search will be and what to do if nothing is searched
 
 //movie search
 if (args[0] === "movie-this") {
   if (args[0] === "movie-this" && args[1] === undefined) {
+    console.log(
+      "----------------------------",
+      "\nPlease Enter A Movie",
+      "\n----------------------------"
+    );
   } else {
     axios
       .get(
         "http://www.omdbapi.com/?s=" + args[1] + "&type=movie&apikey=3f79447c"
       )
-      .then(function(response) {
-        console.log(response.data);
-        let title = response.data[0].title;
-        let year = response.data[0].year;
-        let imdb = response.data[0].imdbID;
+      .then(function (response) {
+        let title = response.data.Search[0].Title;
+        let year = response.data.Search[0].Year;
+        let imdb = response.data.Search[0].imdbID;
         console.log(
           "--------------------",
           "\nTittle: " + title,
